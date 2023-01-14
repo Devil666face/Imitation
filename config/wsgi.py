e981b52c28c463ec0106a422b2f833f07a655ae4
Devil666face <artem1999k@gmail.com>
2023-01-14 17:49:04 +0300

    ipconfig

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,6 +6,11 @@ from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 print(f'Server start on http://{socket.gethostbyname(socket.gethostname())}')
+from sys import platform
+if platform == "linux" or platform == "linux2":
+    os.system('ifconfig')
+elif platform == "win32":
+    os.system('ipconfig')
 application = get_wsgi_application()
 application = WhiteNoise(application, root=STATIC_ROOT, prefix='static/')
 application.add_files(root=MEDIA_ROOT, prefix="media/")
