956003e34b39791c7f7e74aadbd5923b66ec68db
Devil666face <artem1999k@gmail.com>
2023-01-14 20:36:54 +0300

    add start browser

--- config/wsgi.py
+++ config/wsgi.py
@@ -5,12 +5,14 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-print(f'Server start on http://{socket.gethostbyname(socket.gethostname())}')
+host = socket.gethostbyname(socket.gethostname())
+print(f'Server start on http://{host}')
+application = get_wsgi_application()
+application = WhiteNoise(application, root=STATIC_ROOT, prefix='static/')
+application.add_files(root=MEDIA_ROOT, prefix="media/")
 from sys import platform
 if platform == "linux" or platform == "linux2":
     os.system('ifconfig|grep inet')
 elif platform == "win32":
     os.system('ipconfig|findstr IPv4')
-application = get_wsgi_application()
-application = WhiteNoise(application, root=STATIC_ROOT, prefix='static/')
-application.add_files(root=MEDIA_ROOT, prefix="media/")
+    os.system(f'start http://{host}')
