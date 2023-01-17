9de7194cdcca5f966efe23f8c3370d1fce9e2ff1
Artemiy Kalinkin <a.kalinkin@mil.ru>
2023-01-17 09:33:51 +0300

    modify deploy file

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,12 @@
-.\Python38\python -m venv venv
+set "HasArgument="
+for %%a in (%*) do if /i "%%~a"=="-venv" set HasArgument=true
+if defined HasArgument (
+  echo Create venv from local python
+  python -m venv venv
+) else (
+  echo Create venv from source files
+  .\Python38\python -m venv venv
+)
 .\venv\Scripts\pip.exe install --no-index --find-links .\pkg\ -r .\requirements.txt
 .\venv\Scripts\python.exe manage.py collectstatic --no-input
 rem .\venv\Scripts\python.exe manage.py migrate
